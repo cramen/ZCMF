d3abe9d1210e7a6855300d703df604abe58feb99
cramen <cramen@cramen.ru>
2011-03-29 20:57:51 +0400

    Обновил инсталлятор

--- makeinstall.sh
+++ makeinstall.sh
@@ -4,4 +4,6 @@ sudo chmod 777 * -R
 rm www/.tmb/ -rf
 rm www/application/data/cache/* -rf
 rm www/application/data/session/* -rf
+#mysqldump -uroot -proot --compress=true --compact=true --skip-comments --extended-insert=false zcmf > www/application/data/install/dump.sql
+mysqldump -uroot -proot --extended-insert=false zcmf > www/application/data/install/dump.sql
 tar -czf ../zcmf.tar.gz www/
--- www/install/inc/db.php
+++ www/install/inc/db.php
@@ -32,13 +32,25 @@ class install_db
             $dbParam->params->username = $data['username'];
             $dbParam->params->password = $data['password'];
             $dbParam->params->dbname = $data['dbname'];
-            $db = Zend_Db::factory($dbParam);
-
+            //$db = Zend_Db::factory($dbParam);
+	    
             try
             {
-                $db->query('SHOW TABLES;');
+            
+		if (!$conn = mysql_connect($data['host'],$data['username'],$data['password'])) throw new Exception('Connection error');
+		if (!mysql_select_db($data['dbname'],$conn)) throw new Exception('Db select error');
+		mysql_query('SET NAMES UTF8;');
+		
+                //$db->query('SHOW TABLES;');
+                $file = APPLICATION_PATH.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'install'.DIRECTORY_SEPARATOR.'dump.sql';
                 $query = file_get_contents(APPLICATION_PATH.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'install'.DIRECTORY_SEPARATOR.'dump.sql');
-                $db->query($query);
+                $queries = $this->splitQueries($query);
+
+                foreach($queries as $key => $q)
+                {
+                    //$db->query($q);
+                    if (!mysql_query($q,$conn)) throw new Exception('Dump error');
+                }
 
 
                 $config = file_get_contents(APPLICATION_PATH.'/configs/application.ini');
@@ -48,8 +60,7 @@ class install_db
                 $config = preg_replace('/(resources\.db\.params\.dbname\s*=\s*)(.+)/','$1"'.$data['dbname'].'"',$config);
                 file_put_contents(APPLICATION_PATH.'/configs/application.ini',$config);
                 
-
-                return TRUE;
+                return true;
             }
             catch (Exception $e)
             {
@@ -94,5 +105,63 @@ class install_db
         
         return $form;
     }
+
+    /**
+     * Код этой функции взят из CMS Joomla
+     * @return array
+     */
+	protected function splitQueries($sql)
+	{
+        $sql = trim($sql);
+
+        $sql = preg_replace('/(--.*\n)+/','',$sql);
+        $sql = preg_replace('/\/\*!.+\*\/;/','',$sql);
+        $res = preg_split('/;\n/',$sql);
+
+        foreach($res as $key=>$el)
+        {
+            $res[$key] = trim($el,"\n");
+//            echo '::'.$res[$key]."::\n";
+
+            if (substr($el,0,2)=='--') unset($res[$key]);
+
+        }
+
+        return $res;
+        
+//		$buffer		= array();
+//		$queries	= array();
+//		$in_string	= false;
+//
+//		$sql = trim($sql);
+//
+//		$sql = preg_replace("/\n\#[^\n]*/", '', "\n".$sql);
+//
+//		for ($i = 0; $i < strlen($sql) - 1; $i ++)
+//		{
+//			if ($sql[$i] == ";" && !$in_string) {
+//				$queries[] = substr($sql, 0, $i);
+//				$sql = substr($sql, $i +1);
+//				$i = 0;
+//			}
+//
+//			if ($in_string && ($sql[$i] == $in_string) && $buffer[1] != "\\") {
+//				$in_string = false;
+//			}
+//			elseif (!$in_string && ($sql[$i] == '"' || $sql[$i] == "'") && (!isset ($buffer[0]) || $buffer[0] != "\\")) {
+//				$in_string = $sql[$i];
+//			}
+//			if (isset ($buffer[1])) {
+//				$buffer[0] = $buffer[1];
+//			}
+//			$buffer[1] = $sql[$i];
+//		}
+//
+//		if (!empty($sql)) {
+//			$queries[] = $sql;
+//		}
+//
+//		return $queries;
+	}
 
 }
