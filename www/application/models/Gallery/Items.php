41e6fad9397c6146eb778605ee5d52e6fc65819d
cramen <cramen@cramen.ru>
2011-08-01 18:26:40 +0400

    удалил лишние модели

--- www/application/models/Gallery/Items.php
+++
@@ -1,8 +0,0 @@
-<?
-class Site_Model_Gallery_Items extends Z_Db_Table
-{
-
-    protected $_name = 'gallery_items';
-
-
-}
--- www/application/models/News.php
+++
@@ -1,8 +0,0 @@
-<?
-class Site_Model_News extends Z_Db_Table
-{
-
-    protected $_name = 'news';
-
-
-}
