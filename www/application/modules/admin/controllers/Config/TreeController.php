94b90f953fb0cf95cd6243bf7d3a2b6fba7b0904
Cramen <cramen@cramen.ru>
2011-12-08 00:35:57 +0400

    Доделал новую версию конфигураций

--- www/application/modules/admin/controllers/Config/TreeController.php
+++ www/application/modules/admin/controllers/Config/TreeController.php
@@ -3,4 +3,12 @@ class Admin_Config_TreeController extends Z_Admin_Controller_Datacontrol_Abstrac
 {
 
 
+    protected function getTree()
+    {
+        $arr = $this->z_model->fetchPairs(array('id','title'),array('parentid=?'=>0,'type=?'=>'directory'));
+        $arr['0'] = 'Корень';
+        return $arr;
+    }
+
+
 }
--- www/application/modules/admin/controllers/Z/Config/TreeController.php
+++ www/application/modules/admin/controllers/Z/Config/TreeController.php
@@ -2,6 +2,19 @@
 class Admin_Z_Config_TreeController extends Z_Admin_Controller_Datacontrol_Abstract
 {
 
+    public function editSuccess($param)
+    {
+        if ($param['type']=='directory')
+        {
+            $elements = $this->z_model->fetchAll(array('parentid=?'=>$this->_getParam('id')),'orderid asc');
+            foreach ($elements as $el)
+            {
+                $currentValue = $param['value_'.$el->id];
+                $elements = $this->z_model->update(array('value'=>$currentValue),array('id=?'=>$el->id));
+            }
+        }
+    }
+
 
     protected function getForm(Z_Admin_Form $form)
     {
@@ -9,78 +22,121 @@ class Admin_Z_Config_TreeController extends Z_Admin_Controller_Datacontrol_Abstr
         {
 
             $currentParam = $this->z_model->find($this->_getParam('id'))->current();
-            $type = $currentParam->type;
 
-            if($type=='directory')
+
+            if($currentParam->type=='directory')
             {
-                Z_FlashMessenger::addMessage('Нельзя изменять разделы');
-                $this->ajaxGo($this->view->url(array('id'=>'null','action'=>'list')));
+                $form->addElement(new Z_Admin_Form_Element_Label('confLabel',array(
+                    'label'		=>	$currentParam->title,
+                )));
+
+
+                $elements = $this->z_model->fetchAll(array('parentid=?'=>$this->_getParam('id')),'orderid asc');
+                foreach ($elements as $el)
+                {
+                    $this->addConfigFormElement($form, $el->type, $el->title,array(
+                        'name'      =>  'value_'.$el->id,
+                        'value'     =>  $el->value,
+                        'required'  =>  $el->required,
+                    ));
+                }
+            }
+            else
+            {
+                $this->addConfigFormElement($form, $currentParam->type, $currentParam->title);
             }
 
-            if($type=='int')
-            {
-                $form->addElement(new Z_Admin_Form_Element_Text('value',array(
-                    'label'		=>	$currentParam->title,
-                    'validators'=>	array(
-                        'digits'
-                    ),
-                )));
-            }
-            elseif($type=='string')
-            {
-                $form->addElement(new Z_Admin_Form_Element_Text('value',array(
-                    'label'		=>	$currentParam->title,
-                )));
-            }
-            elseif($type=='password')
-            {
-                $form->addElement(new Z_Admin_Form_Element_Password('value',array(
-                    'label'		=>	$currentParam->title,
-                    'required'	=>	true
-                )));
-            }
-            elseif($type=='bool')
-            {
-                $form->addElement(new Z_Admin_Form_Element_Select('value',array(
-                    'label'		=>	$currentParam->title,
-                    'MultiOptions'=>array('Нет','Да'),
-                )));
-            }
-            elseif($type=='text')
-            {
-                $form->addElement(new Z_Admin_Form_Element_Textarea('value',array(
-                    'label'		=>	$currentParam->title,
-                )));
-            }
-            elseif($type=='file')
-            {
-                $form->addElement(new Z_Admin_Form_Element_File('value',array(
-                    'label'		=>	$currentParam->title,
-                    'validators'=>	array(
-                        array('Size', false, 1024*1024*5),
-                    ),
-                )));
-            }
-            elseif($type=='image')
-            {
-                $form->addElement(new Z_Admin_Form_Element_File('value',array(
-                    'label'		=>	$currentParam->title,
-                    'validators'=>	array(
-                        array('Size', false, 1024*1024*5),
-                        array('IsImage'),
-                    ),
-                )));
-            }
-            elseif($type=='html')
-            {
-                $form->addElement(new Z_Admin_Form_Element_Mce('value',array(
-                    'label'		=>	$currentParam->title,
-                    'toolbar'	=>	'default',
-                    'filemanager'	=>	true,
-                )));
-            }
+
         }
 
     }
+
+    protected function addConfigFormElement(Z_Admin_Form $form, $type, $title, $params = array())
+    {
+        $required   = isset($params['required'])?$params['required']:false;
+        $name      = isset($params['name'])?$params['name']:'value';
+        $value      = isset($params['value'])?$params['value']:null;
+
+
+        if($type=='int')
+        {
+            $form->addElement(new Z_Admin_Form_Element_Text($name,array(
+                'label'		=>	$title,
+                'validators'=>	array(
+                    'digits'
+                ),
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+        elseif($type=='string')
+        {
+            $form->addElement(new Z_Admin_Form_Element_Text($name,array(
+                'label'		=>	$title,
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+        elseif($type=='password')
+        {
+            $form->addElement(new Z_Admin_Form_Element_Password($name,array(
+                'label'		=>	$title,
+                'required'	=>	true,
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+        elseif($type=='bool')
+        {
+            $form->addElement(new Z_Admin_Form_Element_Select($name,array(
+                'label'		=>	$title,
+                'MultiOptions'=>array('Нет','Да'),
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+        elseif($type=='text')
+        {
+            $form->addElement(new Z_Admin_Form_Element_Textarea($name,array(
+                'label'		=>	$title,
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+        elseif($type=='file')
+        {
+            $form->addElement(new Z_Admin_Form_Element_File($name,array(
+                'label'		=>	$title,
+                'validators'=>	array(
+                    array('Size', false, 1024*1024*5),
+                ),
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+        elseif($type=='image')
+        {
+            $form->addElement(new Z_Admin_Form_Element_File($name,array(
+                'label'		=>	$title,
+                'validators'=>	array(
+                    array('Size', false, 1024*1024*5),
+                    array('IsImage'),
+                ),
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+        elseif($type=='html')
+        {
+            $form->addElement(new Z_Admin_Form_Element_Mce($name,array(
+                'label'		=>	$title,
+                'toolbar'	=>	'default',
+                'filemanager'	=>	true,
+                'value'     =>  $value,
+                'required'     =>  $required,
+            )));
+        }
+    }
+
 
 }
